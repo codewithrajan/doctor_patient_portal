30f43924ca7bc85c06d3b66baab7868927b20228
Rajan Kumar <rajanranjan06254@gmail.com>
2023-08-22 21:42:54 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div style="text-align: center;font-size:27px;">All Rights Reserved &copy;<?php echo date("Y");?> Developed By Rajan Singh</div>
+</body>
+</html>
+
